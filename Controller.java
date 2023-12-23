1096b95baddec44751701f671ad0190ef8952323
Nickchaching <nicholaslching@gmail.com>
2023-12-23 11:32:02 -0500

    MVC Overhaul Part 2

--- Controller.java
+++ Controller.java
@@ -3,10 +3,19 @@ import java.awt.event.*;
 public class Controller{
     //Properties
     View theView = new View(this);
+    Model theModel = new Model(this);
 
     //Methods
     void viewInteracted(ActionEvent evt){
 
+    }
+
+    void mouseInteracted(MouseEvent evt){
+
+    }
+
+    void keysInteracted(KeyEvent evt){
+        
     }
 
     //Constructor
