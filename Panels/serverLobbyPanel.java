339bf3bedfb650d0dd029b6a57b5e9d69a9d4680
Nickchaching <nicholaslching@gmail.com>
2023-12-23 22:14:18 -0500

    Initial Creation of ServerLobbyPanel

---
+++ Panels/serverLobbyPanel.java
@@ -0,0 +1,14 @@
+//Associating under the Panels Directory (or package)
+package Panels;
+
+//Importing Graphics Dependencies
+import java.awt.*;
+import javax.swing.*;
+
+public class serverLobbyPanel extends JPanel{
+    //Properties
+
+    //Methods
+
+    //Constructor
+}
