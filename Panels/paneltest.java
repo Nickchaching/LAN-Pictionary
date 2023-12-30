77d7bea39d72d61dd2efd17f0d931713978c690f
Nickchaching <nicholaslching@gmail.com>
2023-12-29 23:42:26 -0500

    Created a program to test panels

---
+++ Panels/paneltest.java
@@ -0,0 +1,20 @@
+package Panels;
+import java.awt.*;
+import javax.swing.*;
+
+public class paneltest {
+    JFrame theFrame = new JFrame();
+    serverLobbyPanel testPanel = new serverLobbyPanel();
+
+    public paneltest(){
+        theFrame.setContentPane(testPanel);
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        theFrame.setResizable(true);
+        theFrame.pack();
+        theFrame.setVisible(true);
+    }
+
+    public static void main(String[] args){
+        new paneltest();
+    }
+}
