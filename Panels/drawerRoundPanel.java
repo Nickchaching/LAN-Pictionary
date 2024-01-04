744a27385f9694e70bdc8bcd19cb9d0a89014772
erin-hu24 <erinjhu@gmail.com>
2024-01-03 21:28:29 -0500

    Implemented Array to Save Drawings After Repaints

--- Panels/drawerRoundPanel.java
+++ Panels/drawerRoundPanel.java
@@ -5,13 +5,10 @@ package Panels;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionListener;
-import java.awt.Graphics;
 
 import javax.swing.*;
 
-public class drawerRoundPanel extends JPanel implements ActionListener, MouseMotionListener{
+public class drawerRoundPanel extends JPanel implements ActionListener{
     //Properties
     JButton ClearButton = new JButton("X");
     JButton SSizeButton = new JButton("✎");
@@ -33,55 +30,15 @@ public class drawerRoundPanel extends JPanel implements ActionListener, MouseMot
     public void actionPerformed(ActionEvent evt){
         if(evt.getSource() == theTimer){
             repaint();
-        }
-        else if(evt.getSource() == ClearButton){
-            System.out.println("Clear");
-            blnInitialBackground = false;
-            blnClearPressed = true;
-            System.out.println("blnInitialBackground: "+blnInitialBackground);
-            System.out.println("blnClearPressed: "+blnClearPressed);
-        }
-        else if(evt.getSource() == SSizeButton){
-            paneltest.intPenSize = 3;
-        }
-        else if(evt.getSource() == LSizeButton){
-            paneltest.intPenSize = 5;
-        }
-        else if(evt.getSource() == YellowButton){
-            clrSelected = assets.clrDrawYellow;
-        }
-        else if(evt.getSource() == GreenButton){
-            clrSelected = assets.clrDrawGreen;
-        }
-        else if(evt.getSource() == BlueButton){
-            clrSelected = assets.clrDrawBlue;
-        }
-        else if(evt.getSource() == PurpleButton){
-            clrSelected = assets.clrDrawPurple;
-        }
-        else if(evt.getSource() == RedButton){
-            clrSelected = assets.clrDrawPurple;
-        }
-        else if(evt.getSource() == OrangeButton){
-            clrSelected = assets.clrDrawOrange;
-        }
-        else if(evt.getSource() == BlackButton){
-            clrSelected = assets.clrDrawBlack;
+            paneltest.redraw(paneltest.intDraw, paneltest.intCount);
         }
     }
-    public void mouseDragged(MouseEvent evt){
-        Graphics g = getGraphics();
-        g.setColor(clrSelected);
-        g.fillOval(evt.getX(), evt.getY(), paneltest.intPenSize, paneltest.intPenSize);
-    }
-    public void mouseMoved(MouseEvent evt){
 
-    }
 
 
     public void paintComponent(Graphics g){
-        System.out.println("blnInitialBackground: "+blnInitialBackground);
-        System.out.println("blnClearPressed: "+blnClearPressed);
+        //System.out.println("blnInitialBackground: "+blnInitialBackground);
+        //System.out.println("blnClearPressed: "+blnClearPressed);
         g.setColor(assets.clrBackground);
         g.fillRect(0, 0, 1280, 720);
         g.setColor(assets.clrWhite);
@@ -178,18 +135,7 @@ public class drawerRoundPanel extends JPanel implements ActionListener, MouseMot
         BlackButton.setBackground(assets.clrDrawBlack);
         BlackButton.setBorder(null);
 
-        ClearButton.addActionListener(this);
-        SSizeButton.addActionListener(this);
-        LSizeButton.addActionListener(this);
-        YellowButton.addActionListener(this);
-        GreenButton.addActionListener(this);
-        BlueButton.addActionListener(this);
-        PurpleButton.addActionListener(this);
-        RedButton.addActionListener(this);
-        OrangeButton.addActionListener(this);
-        BlackButton.addActionListener(this);
-
-        addMouseMotionListener(this);
+  
         
         add(ClearButton);
         add(SSizeButton);
@@ -201,5 +147,7 @@ public class drawerRoundPanel extends JPanel implements ActionListener, MouseMot
         add(RedButton);
         add(OrangeButton);
         add(BlackButton);
+
+        theTimer.start();
     }
 }
--- Panels/paneltest.java
+++ Panels/paneltest.java
@@ -1,28 +1,161 @@
 package Panels;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
+import java.awt.Graphics;
 
 
-public class paneltest{
+
+public class paneltest implements ActionListener, MouseMotionListener{
     //Properties
     JFrame theFrame = new JFrame();
-    drawerRoundPanel testPanel = new drawerRoundPanel();
-    
+    static drawerRoundPanel theDrawerRoundPanel = new drawerRoundPanel();
     //Drawing Properties
-    static int intPenSize; 
-        //3 is small, 5 is large
-    static int intPenColour; 
-        //ended up using clrSelected on drawerRoundPanel
-    
+    int intPenSize; //6 is small, 10 is large
+    int intPenColour; 
+    //Saving Drawings After Repaints
+    static int intCount = 0; 
+    static int intDraw[][] = new int[536520][4]; //Array Columns: X, Y, Pen Size, Pen Colour
+    //Variable to Determine if Mouse/Drawing Data Should be Tracked in Array
+    boolean blnSizeChosen = false;
+    boolean blnColourChosen = false; //If both are true, the mouse/data will be stored
 
     //Methods
+    public void actionPerformed(ActionEvent evt){
+        if(evt.getSource() == theDrawerRoundPanel.ClearButton){
+            System.out.println("Clear");
+            //blnInitialBackground = false;
+            //blnClearPressed = true;
+            //System.out.println("blnInitialBackground: "+blnInitialBackground);
+            //System.out.println("blnClearPressed: "+blnClearPressed);
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.SSizeButton){
+            intPenSize = 6;
+            blnSizeChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.LSizeButton){
+            intPenSize = 10;
+            blnSizeChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.YellowButton){
+            theDrawerRoundPanel.clrSelected = assets.clrDrawYellow;
+            intPenColour = 1;
+            blnColourChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.GreenButton){
+            theDrawerRoundPanel.clrSelected = assets.clrDrawGreen;
+            intPenColour = 2;
+            blnColourChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.BlueButton){
+            theDrawerRoundPanel.clrSelected = assets.clrDrawBlue;
+            intPenColour = 3;
+            blnColourChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.PurpleButton){
+            theDrawerRoundPanel.clrSelected = assets.clrDrawPurple;
+            intPenColour = 4;
+            blnColourChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.RedButton){
+            theDrawerRoundPanel.clrSelected = assets.clrDrawRed;
+            intPenColour = 5;
+            blnColourChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.OrangeButton){
+            theDrawerRoundPanel.clrSelected = assets.clrDrawOrange;
+            intPenColour = 6;
+            blnColourChosen = true;
+        }
+        else if(evt.getSource() == theDrawerRoundPanel.BlackButton){
+            theDrawerRoundPanel.clrSelected = assets.clrDrawBlack;
+            intPenColour = 7;
+            blnColourChosen = true;
+        }
+    } 
+    public void mouseDragged(MouseEvent evt){
+        int intX = evt.getX();
+        int intY = evt.getY();
+        if(intX > 15 && intX < 800 && intY > 25 && intY < 695 && blnSizeChosen == true && blnColourChosen == true){
+            //Draw on Drawing Surface of the Panel
+            Graphics g = theDrawerRoundPanel.getGraphics();
+            g.setColor(theDrawerRoundPanel.clrSelected);
+            g.fillOval(intX, intY, intPenSize, intPenSize);
+            //Save Drawing Data in Array
+            intDraw[intCount][0] = intX; 
+            intDraw[intCount][1] = intY; 
+            intDraw[intCount][2] = intPenSize;
+            intDraw[intCount][3] = intPenColour;
+            System.out.println("intCount: " + intCount);
+            System.out.println("X: " + intDraw[intCount][0]);
+            System.out.println("Y: " + intDraw[intCount][1]);
+            System.out.println("Size: " + intDraw[intCount][2]);
+            System.out.println("Colour: " + intDraw[intCount][3]);
+            //Increase intCount
+            intCount++;
+        }    
+    }
+    public void mouseMoved(MouseEvent evt){
 
+    }
+    //Draw Saved Data After Each Repaint
+    public static void redraw(int[][] intDraw, int intCount){
+        //Variables
+        int intX = intDraw[intCount][0];
+        int intY = intDraw[intCount][1];
+        int intSize = intDraw[intCount][2];
+        int intColour = intDraw[intCount][3];
+        Color clrUsed = assets.clrDrawBlack; //Initialized to Prevent Error
+        //Convert Colour Integer to Colour 
+        if(intColour == 1){
+            clrUsed = assets.clrDrawYellow;
+        }
+        else if(intColour == 2){
+            clrUsed = assets.clrDrawGreen;
+        }
+        else if(intColour == 3){
+            clrUsed = assets.clrDrawBlue;
+        }
+        else if(intColour == 4){
+            clrUsed = assets.clrDrawPurple;
+        }
+        else if(intColour == 5){
+            clrUsed = assets.clrDrawRed;
+        }
+        else if(intColour == 6){
+            clrUsed = assets.clrDrawOrange;
+        }
+        else if(intColour == 7){
+            clrUsed = assets.clrDrawBlack;
+        }
+        //Graphics
+        Graphics g = theDrawerRoundPanel.getGraphics();
+        g.setColor(clrUsed);
+        g.fillOval(intX, intY, intSize, intSize);
+    }
+
+    //Constructor
     public paneltest(){
-        theFrame.setContentPane(testPanel);
+        theFrame.setContentPane(theDrawerRoundPanel);
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         theFrame.setResizable(true);
         theFrame.pack();
         theFrame.setVisible(true);
+
+        theDrawerRoundPanel.ClearButton.addActionListener(this);
+        theDrawerRoundPanel.SSizeButton.addActionListener(this);
+        theDrawerRoundPanel.LSizeButton.addActionListener(this);
+        theDrawerRoundPanel.YellowButton.addActionListener(this);
+        theDrawerRoundPanel.GreenButton.addActionListener(this);
+        theDrawerRoundPanel.BlueButton.addActionListener(this);
+        theDrawerRoundPanel.PurpleButton.addActionListener(this);
+        theDrawerRoundPanel.RedButton.addActionListener(this);
+        theDrawerRoundPanel.OrangeButton.addActionListener(this);
+        theDrawerRoundPanel.BlackButton.addActionListener(this);
+
+        theDrawerRoundPanel.addMouseMotionListener(this);
     }
 
     public static void main(String[] args){
