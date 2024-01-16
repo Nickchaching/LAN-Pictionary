52b01b29c22b47b0f35bab4a8edd980427bbc50a
Nickchaching <nicholaslching@gmail.com>
2024-01-16 18:13:00 -0500

    Created Leaderboard Scroll

--- Panels/drawerRoundPanel.java
+++ Panels/drawerRoundPanel.java
@@ -25,12 +25,14 @@ public class drawerRoundPanel extends JPanel implements ActionListener{
     public JLabel CharLabel = new JLabel("0/50");
     public JTextArea ChatArea = new JTextArea("");
     public JTextField ChatField = new JTextField();
-    public JLabel PlayerLabel = new JLabel("  #1 - 200 PTS: joe");
-    public JLabel PlayerLabel2 = new JLabel("  #2 - 180 PTS: bob");
-    public JLabel PlayerLabel3 = new JLabel("  #3 - 150 PTS: jack");
-    public JLabel PlayerLabel4 = new JLabel("  #4 - 120 PTS: jay");
+    public JPanel PlayersPanel = new JPanel();
+    public JScrollPane PlayersScroll = new JScrollPane(PlayersPanel);
+    public JLabel PlayerLabels[];
+    public JLabel PlayerLabels2[];
     public Timer theTimer = new Timer(1000/60, this);
     int intWidth = 1280;
+    private int intScrollVelo = 2;
+    private int intScrollHeight;
     
     int intDraw[][] = new int[536520][4];
     int intCounter = 0;
@@ -41,8 +43,91 @@ public class drawerRoundPanel extends JPanel implements ActionListener{
     //Methods
     public void actionPerformed(ActionEvent evt){
         if(evt.getSource() == theTimer){
+            if(PlayersPanel.getPreferredSize().getHeight() > 250){
+                if(PlayersScroll.getViewport().getViewPosition().getY() >= intScrollHeight/2){
+                    PlayersScroll.getViewport().setViewPosition(new Point(0, 0));
+                }
+                PlayersScroll.getViewport().setViewPosition(new Point(0, (int)PlayersScroll.getViewport().getViewPosition().getY() + intScrollVelo));
+            }
             repaint();
         }
+    }
+
+    public void updatePlayerList(String strPlayerList[]){
+        int intCount;
+
+        if(strPlayerList.length <= 4){
+            PlayersPanel.setBounds(0, 0, 345, 60 * strPlayerList.length);
+            PlayersPanel.setPreferredSize(new Dimension(345, 60 * strPlayerList.length));
+            PlayerLabels = new JLabel[strPlayerList.length];
+
+            for(intCount = 0; intCount < strPlayerList.length; intCount++){
+                PlayerLabels[intCount] = new JLabel(strPlayerList[intCount]);
+                PlayerLabels[intCount].setSize(345,50);
+                PlayerLabels[intCount].setLocation(0, intCount * 60);
+                PlayerLabels[intCount].setHorizontalAlignment(SwingConstants.CENTER);
+                PlayerLabels[intCount].setFont(assets.fntHelvetica30);
+                PlayerLabels[intCount].setBackground(assets.clrLightGrey);
+                PlayerLabels[intCount].setOpaque(true);
+                PlayersPanel.add(PlayerLabels[intCount]);
+            }
+        }
+        else if(strPlayerList.length == 5){
+            theTimer.start();
+            PlayersPanel.removeAll();
+            PlayersPanel.setBounds(0, 0, 345, 60 * 2 * strPlayerList.length);
+            PlayersPanel.setPreferredSize(new Dimension(345, 60 * 2 * strPlayerList.length));
+            intScrollHeight = (int)PlayersPanel.getPreferredSize().getHeight();
+            PlayerLabels = new JLabel[strPlayerList.length];
+            PlayerLabels2 = new JLabel[strPlayerList.length];
+
+            for(intCount = 0; intCount < strPlayerList.length; intCount++){
+                PlayerLabels[intCount] = new JLabel(strPlayerList[intCount]);
+                PlayerLabels[intCount].setSize(345,50);
+                PlayerLabels[intCount].setLocation(0, intCount * 60);
+                PlayerLabels[intCount].setHorizontalAlignment(SwingConstants.CENTER);
+                PlayerLabels[intCount].setFont(assets.fntHelvetica30);
+                PlayerLabels[intCount].setBackground(assets.clrLightGrey);
+                PlayerLabels[intCount].setOpaque(true);
+                PlayersPanel.add(PlayerLabels[intCount]);
+                PlayerLabels2[intCount] = new JLabel(strPlayerList[intCount]);
+                PlayerLabels2[intCount].setSize(345,50);
+                PlayerLabels2[intCount].setLocation(0, 60 * strPlayerList.length + intCount * 60);
+                PlayerLabels2[intCount].setHorizontalAlignment(SwingConstants.CENTER);
+                PlayerLabels2[intCount].setFont(assets.fntHelvetica30);
+                PlayerLabels2[intCount].setBackground(assets.clrLightGrey);
+                PlayerLabels2[intCount].setOpaque(true);
+                PlayersPanel.add(PlayerLabels2[intCount]);
+            }
+        }
+        else if(strPlayerList.length > 5){
+            PlayersPanel.removeAll();
+            PlayersPanel.setBounds(0, 0, 345, 60 * 2 * strPlayerList.length);
+            PlayersPanel.setPreferredSize(new Dimension(345, 60 * 2 * strPlayerList.length));
+            intScrollHeight = (int)PlayersPanel.getPreferredSize().getHeight();
+            PlayerLabels = new JLabel[strPlayerList.length];
+            PlayerLabels2 = new JLabel[strPlayerList.length];
+
+            for(intCount = 0; intCount < strPlayerList.length; intCount++){
+                PlayerLabels[intCount] = new JLabel(strPlayerList[intCount]);
+                PlayerLabels[intCount].setSize(345,50);
+                PlayerLabels[intCount].setLocation(0, intCount * 60);
+                PlayerLabels[intCount].setHorizontalAlignment(SwingConstants.CENTER);
+                PlayerLabels[intCount].setFont(assets.fntHelvetica30);
+                PlayerLabels[intCount].setBackground(assets.clrLightGrey);
+                PlayerLabels[intCount].setOpaque(true);
+                PlayersPanel.add(PlayerLabels[intCount]);
+                PlayerLabels2[intCount] = new JLabel(strPlayerList[intCount]);
+                PlayerLabels2[intCount].setSize(345,50);
+                PlayerLabels2[intCount].setLocation(0, 60 * strPlayerList.length + intCount * 60);
+                PlayerLabels2[intCount].setHorizontalAlignment(SwingConstants.CENTER);
+                PlayerLabels2[intCount].setFont(assets.fntHelvetica30);
+                PlayerLabels2[intCount].setBackground(assets.clrLightGrey);
+                PlayerLabels2[intCount].setOpaque(true);
+                PlayersPanel.add(PlayerLabels2[intCount]);
+            }
+        }
+        PlayersPanel.repaint();
     }
 
     public String getChatField(){
@@ -66,7 +151,7 @@ public class drawerRoundPanel extends JPanel implements ActionListener{
                 String strLine2 = "";
                 while(assets.fntHelvetica13.getStringBounds(strLine1, new FontRenderContext(new AffineTransform(), true, true)).getWidth() >= 300){
                     strLine2 = strLine1.substring(strLine1.lastIndexOf(" ") + 1, strLine1.length()) + " " + strLine2;
-			        strLine1 = strLine1.substring(0, strLine1.lastIndexOf(" "));
+                    strLine1 = strLine1.substring(0, strLine1.lastIndexOf(" "));
                 }
                 ChatArea.append(strLine1 + "\n");
                 ChatArea.append(strLine2 + "\n");
@@ -94,7 +179,7 @@ public class drawerRoundPanel extends JPanel implements ActionListener{
                 String strLine2 = "";
                 while(assets.fntHelvetica13.getStringBounds(strLine1, new FontRenderContext(new AffineTransform(), true, true)).getWidth() >= 300){
                     strLine2 = strLine1.substring(strLine1.lastIndexOf(" ") + 1, strLine1.length()) + " " + strLine2;
-			        strLine1 = strLine1.substring(0, strLine1.lastIndexOf(" "));
+                    strLine1 = strLine1.substring(0, strLine1.lastIndexOf(" "));
                 }
                 ChatArea.append(strLine1 + "\n");
                 ChatArea.append(strLine2 + "\n");
@@ -134,7 +219,7 @@ public class drawerRoundPanel extends JPanel implements ActionListener{
                 String strLine2 = "";
                 while(assets.fntHelvetica13.getStringBounds(strLine1, new FontRenderContext(new AffineTransform(), true, true)).getWidth() >= 300){
                     strLine2 = strLine1.substring(strLine1.lastIndexOf(" ") + 1, strLine1.length()) + " " + strLine2;
-			        strLine1 = strLine1.substring(0, strLine1.lastIndexOf(" "));
+                    strLine1 = strLine1.substring(0, strLine1.lastIndexOf(" "));
                 }
                 ChatArea.append(strLine1 + "\n");
                 ChatArea.append(strLine2 + "\n");
@@ -352,34 +437,14 @@ public class drawerRoundPanel extends JPanel implements ActionListener{
         ChatField.setBackground(assets.clrWhite);
         ChatField.setFont(assets.fntHelvetica15);
 
-        PlayerLabel.setSize(345, 50);
-        PlayerLabel.setLocation(910, 35);
-        PlayerLabel.setHorizontalAlignment(SwingConstants.LEFT);
-        PlayerLabel.setFont(assets.fntHelvetica20);
-        PlayerLabel.setBackground(assets.clrLightGrey);
-        PlayerLabel.setOpaque(true);
+        PlayersPanel.setBackground(assets.clrWhite);
+        PlayersPanel.setLayout(null);
+        PlayersScroll.setSize(new Dimension(345, 250));
+        PlayersScroll.setLocation(910, 35);
+        PlayersScroll.setBorder(null);
+        PlayersScroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        PlayersScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
-        PlayerLabel2.setSize(345, 50);
-        PlayerLabel2.setLocation(910, 95);
-        PlayerLabel2.setHorizontalAlignment(SwingConstants.LEFT);
-        PlayerLabel2.setFont(assets.fntHelvetica20);
-        PlayerLabel2.setBackground(assets.clrLightGrey);
-        PlayerLabel2.setOpaque(true);
-
-        PlayerLabel3.setSize(345, 50);
-        PlayerLabel3.setLocation(910, 155);
-        PlayerLabel3.setHorizontalAlignment(SwingConstants.LEFT);
-        PlayerLabel3.setFont(assets.fntHelvetica20);
-        PlayerLabel3.setBackground(assets.clrLightGrey);
-        PlayerLabel3.setOpaque(true);
-
-        PlayerLabel4.setSize(345, 50);
-        PlayerLabel4.setLocation(910, 215);
-        PlayerLabel4.setHorizontalAlignment(SwingConstants.LEFT);
-        PlayerLabel4.setFont(assets.fntHelvetica20);
-        PlayerLabel4.setBackground(assets.clrLightGrey);
-        PlayerLabel4.setOpaque(true);
-        
         add(ClearButton);
         add(SSizeButton);
         add(LSizeButton);
@@ -394,10 +459,7 @@ public class drawerRoundPanel extends JPanel implements ActionListener{
         add(CharLabel);
         add(ChatArea);
         add(ChatField);
-        add(PlayerLabel);
-        add(PlayerLabel2);
-        add(PlayerLabel3);
-        add(PlayerLabel4);
+        add(PlayersScroll);
 
         theTimer.start();
     }
--- Panels/paneltest.java
+++ Panels/paneltest.java
@@ -5,74 +5,32 @@ import java.awt.event.*;
 
 
 
-public class paneltest implements ActionListener, MouseMotionListener{
+public class paneltest{
     //Properties
     JFrame theFrame = new JFrame();
-    drawerRoundPanel theDrawerRoundPanel = new drawerRoundPanel();
+    drawerRoundPanel testPanel = new drawerRoundPanel();
 
-    //Methods
-    public void actionPerformed(ActionEvent evt){
-        if(evt.getSource() == theDrawerRoundPanel.ClearButton){
-            theDrawerRoundPanel.clearScreen();
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.SSizeButton){
-            theDrawerRoundPanel.updateSize(6);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.LSizeButton){
-            theDrawerRoundPanel.updateSize(10);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.YellowButton){
-            theDrawerRoundPanel.updateColour(1);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.GreenButton){
-            theDrawerRoundPanel.updateColour(2);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.BlueButton){
-            theDrawerRoundPanel.updateColour(3);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.PurpleButton){
-            theDrawerRoundPanel.updateColour(4);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.RedButton){
-            theDrawerRoundPanel.updateColour(5);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.OrangeButton){
-            theDrawerRoundPanel.updateColour(6);
-        }
-        else if(evt.getSource() == theDrawerRoundPanel.BlackButton){
-            theDrawerRoundPanel.updateColour(7);
-        }
-    } 
-    public void mouseDragged(MouseEvent evt){
-        int intX = evt.getX();
-        int intY = evt.getY();
-        if(intX > 15 && intX < 800 && intY > 25 && intY < 695){
-            theDrawerRoundPanel.updateDraw(intX, intY);
-        }
-    }
-    public void mouseMoved(MouseEvent evt){
-
-    }
 
     //Constructor
     public paneltest(){
-        theFrame.setContentPane(theDrawerRoundPanel);
+        theFrame.setContentPane(testPanel);
         theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         theFrame.setResizable(true);
         theFrame.pack();
         theFrame.setVisible(true);
+        
+        String[] strPlayerList = new String[8];
+        strPlayerList[0] = "bob";
+        strPlayerList[1] = "bob1";
+        strPlayerList[2] = "bob2";
+        strPlayerList[3] = "bob3";
+        strPlayerList[4] = "bob4";
+        strPlayerList[5] = "bob5";
+        strPlayerList[6] = "bob6";
+        strPlayerList[7] = "bob7";
 
-        theDrawerRoundPanel.ClearButton.addActionListener(this);
-        theDrawerRoundPanel.SSizeButton.addActionListener(this);
-        theDrawerRoundPanel.LSizeButton.addActionListener(this);
-        theDrawerRoundPanel.YellowButton.addActionListener(this);
-        theDrawerRoundPanel.GreenButton.addActionListener(this);
-        theDrawerRoundPanel.BlueButton.addActionListener(this);
-        theDrawerRoundPanel.PurpleButton.addActionListener(this);
-        theDrawerRoundPanel.RedButton.addActionListener(this);
-        theDrawerRoundPanel.OrangeButton.addActionListener(this);
-        theDrawerRoundPanel.BlackButton.addActionListener(this);
-        theDrawerRoundPanel.addMouseMotionListener(this);
+
+        testPanel.updatePlayerList(strPlayerList);
     }
 
     public static void main(String[] args){
